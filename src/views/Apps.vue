76fcf3fbd4b5a8cc21d9a3d9531c9bf7d0699b4b
mghako <mghakofreelancer@gmail.com>
2020-05-13 23:04:59 +0630

    changed mobile view for app page

--- src/views/Apps.vue
+++ src/views/Apps.vue
@@ -2,8 +2,8 @@
   <div class="container w-10/12 mx-auto flex justify-between mt-12">
     <!-- vuejs app code block-->
     <div class="w-3/6 p-6 shadow">
-      <h2 class="text-2xl mb-6 bg-gray-800 text-white">
-        <a href="https://voucher.mghako.com" target="_blank">Voucher App (ongoing status)</a>
+      <h2 class="text-2xl mb-6 ">
+        <a href="https://voucher.mghako.com" target="_blank" class="bg-gray-800 text-white py-6 px-2 rounded">Voucher App (ongoing status)</a>
       </h2>
       <img src="../../public/img/voucher_thumbnails.png" alt class="block w-8/10 mx-auto" />
       <p class="mb-4 mt-2">Voucher printing အတွက် vuejs နဲ့ရေးထားသော light weight app ဖြစ်ပါတယ်။</p>
@@ -18,8 +18,8 @@
         <i class="fas fa-eye"></i> Live Preview
       </a>
     </div>
-    <div class="w-3/6 bg-green-500 text-white">Coming Soon!</div>
-    <div class="w-3/6 bg-green-600 text-white">Coming Soon!</div>
+    <!-- <div class="w-3/6 bg-green-500 text-white">Coming Soon!</div>
+    <div class="w-3/6 bg-green-600 text-white">Coming Soon!</div> -->
   </div>
 </template>
 
