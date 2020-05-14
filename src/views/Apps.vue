3c9e84628ce09163f7a6d9bec1525e490d4e7ee8
yami web <yamiofweb@protonmail.com>
2020-05-14 21:35:11 +0630

    changed view of app

--- src/views/Apps.vue
+++ src/views/Apps.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="container w-10/12 mx-auto flex justify-between mt-12">
     <!-- vuejs app code block-->
-    <div class="w-full p-6 shadow">
+    <div class="md:w-1/2 sm:w-full p-6 shadow md:h-auto">
       <h2 class="text-2xl mb-6 ">
         <a href="https://voucher.mghako.com" target="_blank" class="block bg-gray-800 text-white py-6 px-2">Voucher App</a>
         <small class="block bg-green-400 text-center">(ongoing status)</small>
       </h2>
-      <img src="../../public/img/voucher_thumbnails.png" alt class="block w-8/10 mx-auto" />
+      <img src="../../public/img/voucher_thumbnails.png" alt class="block w-8/10 mx-auto h-64" />
       <p class="mb-4 mt-2">Voucher printing အတွက် vuejs နဲ့ရေးထားသော light weight app ဖြစ်ပါတယ်။</p>
       <a
         href="https://github.com/mghako/voucher_app"
@@ -15,12 +15,30 @@
       >
         <i class="fas fa-code"></i> Source
       </a>
-      <a href class="inline-block bg-gray-700 px-4 py-1 text-white rounded">
+      <a href="https://voucher.mghako.com" target="_blank" class="inline-block bg-gray-700 px-4 py-1 text-white rounded">
         <i class="fas fa-eye"></i> Live Preview
       </a>
     </div>
-    <!-- <div class="w-3/6 bg-green-500 text-white">Coming Soon!</div>
-    <div class="w-3/6 bg-green-600 text-white">Coming Soon!</div> -->
+    <!-- vuejs app code block-->
+    <div class="md:w-1/2 sm:w-full p-6 shadow md:h-auto">
+      <h2 class="text-2xl mb-6 ">
+        <a href="https://notiboard.mghako.com" target="_blank" class="block bg-gray-800 text-white py-6 px-2">Notiboard App</a>
+        <small class="block bg-green-400 text-center">(ongoing status)</small>
+      </h2>
+      <img src="../../public/img/notiboard_thumbnails.png" alt class="block w-8/10 mx-auto md:h-64" />
+      <p class="mb-4 mt-2">ကိုယ်ေရးထားတဲ့ App ကို notiboard မွာ ပျန္လည္မ်ွေၽေပးတဲ့ App </p>
+      <a
+        href="https://github.com/mghako/voucher_app"
+        target="_blank"
+        class="inline-block bg-gray-700 px-4 py-1 text-white rounded mr-4 mb-4"
+      >
+        <i class="fas fa-code"></i> Source
+      </a>
+      <a href="https://voucher.mghako.com" target="_blank" class="inline-block bg-gray-700 px-4 py-1 text-white rounded">
+        <i class="fas fa-eye"></i> Live Preview
+      </a>
+    </div>
+    
   </div>
 </template>
 
